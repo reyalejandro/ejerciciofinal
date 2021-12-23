59e49a4bdfddc5c10de68d7344aa669afe252448
Alejandro Rey <74797718+reyalejandro@users.noreply.github.com>
2021-12-23 05:50:47 +0100

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -2,6 +2,6 @@
 echo "****************"
 echo "* Building jar!*"
 echo "****************"
-PROJ=/home/ricardo/jenkins/jenkins_home/workspace/pipeline-docker-maven
-docker run --rm -v /root/.m2:/root/.m2 -v /home/alejandro/jenkins/pipeline/java-app:/app -w /app maven:3-alpine "$@"
+PROJ=/home/alejandro/jenkins/pipeline
+docker run --rm -v /root/.m2:/root/.m2 -v $PROJ/java-app:/app -w /app maven:3-alpine "$@"
 

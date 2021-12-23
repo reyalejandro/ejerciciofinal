175219b992e545b96aa900d99c792118c76a7a56
Alejandro Rey <74797718+reyalejandro@users.noreply.github.com>
2021-12-23 05:38:25 +0100

    Update test.sh

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,5 +4,5 @@ echo "################"
 echo "*** Testing ***"
 echo "################"
 
-docker run --rm -v /root/.m2:/root/.m2 -v $PWD/java-app:/app -w /app maven:3-alpine "$@"
+docker run --rm -v /root/.m2:/root/.m2 -v /home/alejandro/jenkins/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:3-alpine "$@"
 

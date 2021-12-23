493a86d8ddf1fbcacf72fc0d35d7496ef6099717
Alejandro Rey <74797718+reyalejandro@users.noreply.github.com>
2021-12-23 06:01:57 +0100

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,4 +7,3 @@ echo $BUILD_TAG >> /tmp/.auth
 scp -i /tmp/tf_pipeline/class_key.pem /tmp/.auth ubuntu@ec2-3-142-164-78.us-east-2.compute.amazonaws.com:/tmp/.auth
 scp -i /tmp/tf_pipeline/class_key.pem /tmp/jenkins/deploy/publish ubuntu@ec2-3-142-164-78.us-east-2.compute.amazonaws.com:/tmp/publish
 ssh -i /tmp/tf_pipeline/class_key.pem ubuntu@ec2-3-142-164-78.us-east-2.compute.amazonaws.com /tmp/publish
-
